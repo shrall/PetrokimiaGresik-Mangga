75a55eaa331a7079a28165eef5eb0da3f42879f8
shrall <shrallvierdo@gmail.com>
2021-10-01 16:25:34 +0700

    database - indonesia

--- database/seeders/IndoRegionSeeder.php
+++ database/seeders/IndoRegionSeeder.php
@@ -1,10 +1,17 @@
 <?php
 
+/*
+ * This file is part of the IndoRegion package.
+ *
+ * (c) Azis Hapidin <azishapidin.com | azishapidin@gmail.com>
+ *
+ */
+
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class IndoRegionSeeder extends Seeder
 {
     /**
      * Seed the application's database.
@@ -13,7 +20,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
         $this->call(IndoRegionProvinceSeeder::class);
         $this->call(IndoRegionRegencySeeder::class);
         $this->call(IndoRegionDistrictSeeder::class);
