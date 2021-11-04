03fa78956558e333fc11065fb9f0d5db4d5d7dba
shrall <shrallvierdo@gmail.com>
2021-11-05 02:07:48 +0700

    landing page - info - responsive

--- resources/views/landing_page/info.blade.php
+++ resources/views/landing_page/info.blade.php
@@ -5,30 +5,30 @@
         Kecil<br>(PUMK)</div>
     <div class="text-center text-mangga-green-400 font-os text-xl mb-16">Program untuk meningkatkan kemampuan usaha mikro dan
         usaha kecil agar menjadi tangguh dan mandiri</div>
-    <div class="grid grid-cols-4 gap-x-4 mx-28 mb-24">
-        <div class="border border-gray-400 text-center bg-white font-os text-xl pb-8">
-            <div class="fa fa-fw fa-book-open text-mangga-orange-400 text-9xl my-8"></div><br>
+    <div class="grid grid-cols-1 md:grid-cols-4 gap-4 mx-24 md:mx-12 xl:mx-28 mb-24">
+        <div class="border border-gray-400 text-center bg-white font-os text-md xl:text-xl pb-8">
+            <div class="fa fa-fw fa-book-open text-mangga-orange-400 text-8xl xl:text-9xl my-8"></div><br>
             INSTRUKSI KERJA<br>
             MONITORING PINJAMAN<br>
             MODAL KERJA<br>
             BAGI MITRA BINAAN
         </div>
-        <div class="border border-gray-400 text-center bg-white font-os text-xl pb-8">
-            <div class="fa fa-fw fa-book text-mangga-green-500 text-9xl my-8"></div><br>
+        <div class="border border-gray-400 text-center bg-white font-os text-md xl:text-xl pb-8">
+            <div class="fa fa-fw fa-book text-mangga-green-500 text-8xl xl:text-9xl my-8"></div><br>
             INSTRUKSI KERJA<br>
             MONITORING PINJAMAN<br>
             MODAL KERJA<br>
             BAGI MITRA BINAAN
         </div>
-        <div class="border border-gray-400 text-center bg-white font-os text-xl pb-8">
-            <div class="fa fa-fw fa-book-open text-mangga-orange-400 text-9xl my-8"></div><br>
+        <div class="border border-gray-400 text-center bg-white font-os text-md xl:text-xl pb-8">
+            <div class="fa fa-fw fa-book-open text-mangga-orange-400 text-8xl xl:text-9xl my-8"></div><br>
             INSTRUKSI KERJA<br>
             MEKANISME MENJADI MB<br>
             PROGRAM<br>
             PUMK 2021
         </div>
-        <div class="border border-gray-400 text-center bg-white font-os text-xl pb-8">
-            <div class="fa fa-fw fa-book text-mangga-green-500 text-9xl my-8"></div><br>
+        <div class="border border-gray-400 text-center bg-white font-os text-md xl:text-xl pb-8">
+            <div class="fa fa-fw fa-book text-mangga-green-500 text-8xl xl:text-9xl my-8"></div><br>
             INSTRUKSI KERJA<br>
             MEKANISME MENJADI MB<br>
             PROGRAM<br>
