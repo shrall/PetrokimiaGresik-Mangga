91429e6876c12586dab8184e8a4a47caf9949b81
shrall <shrallvierdo@gmail.com>
2022-02-01 09:17:13 +0700

    Update snappy.php

--- config/snappy.php
+++ config/snappy.php
@@ -35,8 +35,8 @@ return [
 
     'pdf' => [
         'enabled' => true,
-        // 'binary'  => base_path('vendor/h4cc/wkhtmltopdf-amd64/bin/wkhtmltopdf-amd64'),
-        'binary' => public_path('laravel-snappy/wkhtmltopdf.exe'),
+        'binary'  => base_path('vendor/h4cc/wkhtmltopdf-amd64/bin/wkhtmltopdf-amd64'),
+        // 'binary' => public_path('laravel-snappy/wkhtmltopdf.exe'),
         'timeout' => false,
         'options' => [],
         'env'     => [],
