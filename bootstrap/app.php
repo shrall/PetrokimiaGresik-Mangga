a6d43a9022ba375f76e371c6810c1b7348d0b182
Marshall Ovierdo <shrallvierdo@gmail.com>
2022-02-11 15:28:26 +0700

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,6 +15,10 @@ $app = new Illuminate\Foundation\Application(
     $_ENV['APP_BASE_PATH'] ?? dirname(__DIR__)
 );
 
+$app->bind('path.public', function() {
+    return realpath(__DIR__.'/../public_html');
+});
+
 /*
 |--------------------------------------------------------------------------
 | Bind Important Interfaces
